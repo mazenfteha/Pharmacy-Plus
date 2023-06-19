a5f1b5f960ee71a48437ed9cfe753b8ed9afe585
mazenfteha <mazn34903@gmail.com>
2023-06-18 20:50:58 -0700

    Setup routes and  connect to db

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express')
-
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Pharmacy Plus')
 })
 
+//routers
+const authRouter = require('./routes/auth')
+
+app.use('/api/v1/auth',authRouter)
+
+//connect to db
+const connectDB =require('./db/connect')
+
 const PORT = process.env.PORT || 80 ;
-app.listen(PORT, () =>
-console.log(`Server is listening on port ${PORT}...`))
+const start = async () => {
+    try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () =>
+        console.log(`Server is listening on port ${PORT}...`)
+    );
+    } catch (error) {
+    console.log(error);
+    }
+};
+
+start();
